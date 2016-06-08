e9ad000cdb48b4a3c42d8474309ee7c123b921b0
lidia <lgliducik@gmail.com>
2016-06-08 09:04:46 +0400

    delete unuse line

--- ma.py
+++ ma.py
@@ -21,26 +21,24 @@ class MyError(Exception):
 
 class RevealAccess(object):
 
-    def __init__(self, initval=None, name='var'):
+    def __init__(self, initval=None):
         self.val = initval
-        self.name = name
 
     def __get__(self, obj, objtype):
         return self.val
 
     def __set__(self, obj, val):
         if val == 2:
-            raise AttributeError("isSecond = 2")
+            raise AttributeError()
         self.val = val
 
 
 class A(First, Parent, object):
     i = 0
-    isSecond = RevealAccess(0, 'var "isSecond"')
+    isSecond = RevealAccess(0)
 
     def __init__(self):
         self.i = 3
-        self.isSecond = 0
 
     def isFirst(self):
         if self.isSecond == 0:
